d9f4e3d656921ac581ab80bb05f77b25e8166317
mastervec <victor.pads@gmail.com>
2026-06-03 15:43:08 -0300

    lab1

---
+++ lab1_arquivos/L__1_img.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('messi5.jpg')
+#remove parametro que tira a cor
+cv.imshow('image',img)
+k = cv.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv.imwrite('messigray.png',img)
+    cv.destroyAllWindows()
